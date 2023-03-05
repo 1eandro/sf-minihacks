bc0507f28fe740322ef6fe22c3f0068d5207473f
Leandro Fernandes <leandro.f.smart@gmail.com>
2023-03-05 14:37:52 +0100

    adding timeout of 200ms to debounce

--- force-app/main/default/lwc/slider/slider.js
+++ force-app/main/default/lwc/slider/slider.js
@@ -3,15 +3,19 @@ import { LightningElement } from "lwc";
 export default class LightningExampleSliderBasic extends LightningElement {
   val = 2;
   clicks = 0;
+  timer;
 
   handleChange(event) {
+    clearTimeout(this.timer);
     const targetValue = event.target.value;
-    const curEvent = new CustomEvent("sliderclicked", {
-      detail: {
-        count: targetValue,
-        clicks: ++this.clicks
-      },
-    });
-    this.dispatchEvent(curEvent);
+    this.timer = setTimeout(() => {
+      const curEvent = new CustomEvent("sliderclicked", {
+        detail: {
+          count: targetValue,
+          clicks: ++this.clicks,
+        },
+      });
+      this.dispatchEvent(curEvent);
+    }, 200);
   }
 }
