a240bae4f7b35497d27785655a2ad9eb3cdf2c62
Leandro Fernandes <leandro.f.smart@gmail.com>
2023-03-05 14:22:59 +0100

    working 1.0, but it is still updating the slider for every microscopic change. We need to implement the debouncing logic to prevent that

--- force-app/main/default/lwc/slider/slider.js
+++ force-app/main/default/lwc/slider/slider.js
@@ -2,4 +2,16 @@ import { LightningElement } from "lwc";
 
 export default class LightningExampleSliderBasic extends LightningElement {
   val = 2;
+  clicks = 0;
+
+  handleChange(event) {
+    const targetValue = event.target.value;
+    const curEvent = new CustomEvent("sliderclicked", {
+      detail: {
+        count: targetValue,
+        clicks: ++this.clicks
+      },
+    });
+    this.dispatchEvent(curEvent);
+  }
 }
